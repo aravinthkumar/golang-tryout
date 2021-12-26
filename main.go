bf9cde816653fc8173c9cc0a1c9aae7d37469f43
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2021-12-26 08:45:04 +0530

    for loop with till and break

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello, Gophers")
-	port := 3000
-	// function invoked with argument
-	b, err := startWebServer(port, "2")
-	fmt.Println(b, err)
-}
+	//for loop with till condition
+	var i int
+	for i < 5 {
+		println(i)
+		i++
+		if i == 3 {
+			break
+		}
 
-// Function with input parametes
-func startWebServer(port int, numberOfRetries string) (bool, error) {
-
-	fmt.Println("Starting server ....")
-
-	fmt.Println("Number of time retried", numberOfRetries)
-	// do something
-	fmt.Println("Server started in port", port)
-
-	return true, nil
-
+	}
 }
