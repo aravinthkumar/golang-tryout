55e79d388d5e7bf8c31594fdc21640880fc996a1
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-01-04 19:26:47 +0530

    Buffered Channel

--- main.go
+++ main.go
@@ -7,15 +7,18 @@ import (
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	// Buffered Channel with value 1
+	ch := make(chan int, 1)
 
 	wg.Add(2)
 	go func(ch chan int, wg *sync.WaitGroup) {
-		fmt.Println(<-ch)
+		ch <- 42
+		// This is ignored since buffer is 1
+		ch <- 49
 		wg.Done()
 	}(ch, wg)
 	go func(ch chan int, wg *sync.WaitGroup) {
-		ch <- 42
+		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 	wg.Wait()
